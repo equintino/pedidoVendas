f0c46b592d63f93aad9307d10dc20338c9d33faa
equintino <emquintino@ig.com.br>
2018-03-22 15:45:15 -0300

    banco cliente

--- paginas/cliente.php
+++ paginas/cliente.php
@@ -37,7 +37,7 @@
     }
     
 ////////// Clientes ////////////
-   if($act=='list'){    
+   if($act=='list'){   
         //// Listar Clientes ////
         $clientes_list_request=array('pagina'=>$pagAtual,'registros_por_pagina'=>'100','apenas_importado_api'=>'N');
         $dados=$cliente->ListarClientes($clientes_list_request);
@@ -49,15 +49,26 @@
    }elseif($act=='atualiza'){
         $clientes_list_request=array('pagina'=>'1','registros_por_pagina'=>'100','apenas_importado_api'=>'N');
         $dados=$cliente->ListarClientes($clientes_list_request);
-        echo $dados->total_de_paginas;
+        $paginas=$dados->total_de_paginas;
         
-        $campos=array('bairro','cep','cidade','cidade_ibge','cnae','cnpj_cpf','codigo_cliente_integracao','codigo_cliente_omie','codigo_pais','complemento','email','endereco','endereco_numero','estado','exterior','inscricao_estadual','inscricao_municipal','nome_fantasia','pessoa_fisica','razao_social','telefone1_ddd','telefone1_numero','tipo_atividade');
+        //echo '<pre>';print_r($dados->clientes_cadastro[0]);die;
+        
+        //$campos=array('bairro','cep','cidade','cidade_ibge','cnae','cnpj_cpf','codigo_cliente_integracao','codigo_cliente_omie','codigo_pais','complemento','email','endereco','endereco_numero','estado','exterior','inscricao_estadual','inscricao_municipal','nome_fantasia','pessoa_fisica','razao_social','telefone1_ddd','telefone1_numero','tipo_atividade');
         $result = array();
         foreach($dados->clientes_cadastro as $row){
             $model = new Model();
-            echo '<pre>';print_r($row);die;
-            //modelMapper::map($model, $row);
+            foreach($row as $key => $item){
+                if($key != 'info' && $key != 'tags'){
+                    $classe='set'.$key;
+                    //echo $classe.'()';die;
+                    $model->$classe($item);
+                }
+            }
+            echo '<pre>';print_r([$model,$row]);die;
+            modelMapper::map($model, $row);
+            echo '<pre>';print_r($model);die;
             $result[$model->getid()] = $model;
+            print_r([$model,$result]);die;
         }
         foreach($_POST as $key => $item){
             $classe='set'.$key;
