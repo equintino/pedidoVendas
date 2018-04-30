cbac82230ec1f9ed59637abc5a6c25f3727a6a4d
equintino <emquintino@ig.com.br>
2018-04-30 14:47:29 -0300

    imprime

--- paginas/imprime.php
+++ paginas/imprime.php
@@ -1,49 +1,17 @@
 <html>
     <head>
         <meta charset="utf-8" />
-        <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+        <script type="text/javascript" src="../web/js/jquery-3.2.1.min.js"></script>
         
         <script>
             $(document).ready(function(){
-                window.print();
-                //window.location='../web/index.php?pagina=pedido&act=cad';
+                if(!flash && numero_pedido){
+                    $('.erro').hide();
+                    window.print();
+                    window.close();
+                    /*window.location='../web/index.php?pagina=pedido&act=cad';*/
+                }
             })
-            
-            /* estudar
-            // Imprime a página
-        window.print();
-                
-        (function () {
-            var beforePrint = function () {
-                
-                // Remove o loading
-                $(".se-pre-con").hide();
-
-                // Remove Menu topo
-                $(".header").hide();
-                $('.content').css({
-                    top: ("0px")
-                }).show();
-            };
-            var afterPrint = function () {
-                // Redireciona página
-                window.location = 'Logado.php?pagina=<?php //$pagina_crypt ?>';
-            };
-
-            if (window.matchMedia) {
-                var mediaQueryList = window.matchMedia('print');
-                mediaQueryList.addListener(function (mql) {
-                    if (mql.matches) {
-                        beforePrint();
-                    } else {
-                        afterPrint();
-                    }
-                });
-            }
-
-            window.onbeforeprint = beforePrint;
-            window.onafterprint = afterPrint;
-        }()); */
         </script>
         <style>
             *{
@@ -56,13 +24,14 @@
                 font-weight: 900;
                 width: 105mm;
                 text-transform: uppercase;
+                line-height: 1.4;
             }
             #texto{
                 width: 100%; 
                 margin: auto;
             }
             .conteudo{
-                width: 85mm;
+                width: 86mm;
                 background: white;
                 padding: 20px;
             }
@@ -71,15 +40,9 @@
             }
             .endereco, .web{
                 text-align: center;
-                //font-size: 10px;
             }
             .cnpj{
                 margin-top: 10px;
-            }
-            .cnpj,.ie{
-                //font-weight: 900;
-                //color: black;
-                //font-size: 13px;
             }
             hr{
                 margin-top: 8px;
@@ -98,28 +61,28 @@
                 margin-top: 5px;
             }
             .descricao{
-                margin-left: 50px;
+                margin-left: 25px;
             }
             .qtd, .quant{
                 margin-left: 20px;
             }
             .vlUnit{
-                margin-left: 60px;
+                margin-left: 30px;
             }
             .vlItem, .vTotalItem2{
                 float: right;
             }
             .cod{
-                margin-left: 35px;
+                margin-left: 30px;
             }
             .desc{
-                margin-left: 65px;
+                margin-left: 30px;
             }
             .quant{
                 margin-right: 20px;
             }
             .vlUnit2{
-                margin-left: 60px;
+                margin-left: 40px;
             }
             .vTotal{
                 float: right;
@@ -135,37 +98,98 @@
                 clear: both;
                 margin-top: 20px;
             }
-            .dCliente{
-                //font-style: italic;
-            }
-            .campos, .dCliente{
-                //font-size: 12px;
-            }
             .rodape{
-                //margin-top: 30px;
                 text-align: center;
+            }
+            .erro{
+                color: red;
+            }
+            .fpagamento{
+                clear: right;
+                float: right;
+                margin-top: 15px;
             }
             
             @media print{
                 .conteudo {
-                    //-webkit-column-count: 1; /* Chrome, Safari, Opera */
-                    //-moz-column-count: 1; /* Firefox */
-                    //column-count: 1;
+                    /*-webkit-column-count: 1; /* Chrome, Safari, Opera */
+                    /*-moz-column-count: 1; /* Firefox */
+                    /*column-count: 1;*/
                 }
             }
             @page{
                 size: auto;
                 margin: 0mm;
             }
+            
+            .erro{
+                font-weight: 900;
+                color: red;
+                animation: blinker 1s linear infinite;
+                text-align: center;
+            }
+            
+            .blink_me {
+            }
+            @keyframes blinker {
+              50% {
+                opacity: 0.3;
+              }
+            }
         </style>
 <?php
+    function reduz($str,$num){
+        if(strlen($str)>$num){
+            $str=substr($str,0,$num).'...';
+        }
+        return $str;
+    }
+    
+    array_key_exists('direto',$_GET)? $direto=$_GET['direto']: $direto=null;
+    
+    @$vendedor=$_POST['vendedor'];
+    @$empresaAtualiza=$_GET['empresaAtualiza'];
+    @$tItens=$pedido_venda_produto->cabecalho->quantidade_itens;
+    @$vPedido=$_POST['vPedido'];
+    @$vDesconto=$_POST['vDesconto'];
+    $cliente=reduz($_POST['cliente'],23);
+    $enderecoCliente=reduz($_POST['endereco'],23);
+    $bairroCliente=reduz($_POST['bairro'],12);
+    $cepCliente=substr($_POST['cep'],0,4).'-'.substr($_POST['cep'],5,3);
+    $cidadeCliente=reduz($_POST['cidade'],25);
+    $cnpj_cpfCliente=$_POST['cnpj_cpf'];
+    
     include '../model/EmpresasCadastroJsonClient.php';
     include '../model/empresa.php';
     
-    @$pedido=$_GET['pedido'];
-    @$vendedor=$_POST['vendedor'];
-    @$empresaAtualiza=$_GET['empresaAtualiza'];
+    if($direto){
+        include '../dao/UserDao.php';
+        include '../dao/UserSearchCriteria.php';
+        include '../config/Config.php';
+        include '../model/User.php';
+        include '../mapping/UserMapper.php';
+        include '../dao/PedidoSearchCriteria.php';
+        include '../model/pedido.php';
+        include '../mapping/pedidoMapper.php';
+        include '../excecao/Excecao.php';
+        include '../dao/dao.php';
+        include '../dao/CRUDPedido.php';
+
+        $user = new UserDao();
+        $search = new UserSearchCriteria();
+        $search->setLogin($_COOKIE['login']);
+        foreach($user->find($search) as $usuario){
+            define('OMIE_APP_KEY',$usuario->getOMIE_APP_KEY());
+            define('OMIE_APP_SECRET',$usuario->getOMIE_APP_SECRET());
+        }
+        $search=new PedidoSearchCriteria();
+        $search->settabela('tb_pedido');
+        $dao=new CRUDPedido();
+        echo '<pre>';
+        print_r($dao->encontrePorPedido($search));
+    }
     
+        
     $empresaAtualiza=1;
     if($empresaAtualiza==1){
         $emp=new EmpresasCadastroJsonClient();
@@ -209,28 +233,20 @@
         $telefone1_numero=$empresa->telefone1_numero;
         $website=$empresa->website;
     }
-    //echo $website;
-    //die;
-    
-    //echo '<pre>';print_r($pedido_venda_produto);
-    @$tItens=$pedido_venda_produto->cabecalho->quantidade_itens;
-    
-    //echo '<pre>';print_r($pedido_venda_produto->det);
-    //print_r($_POST);die;
-    //@$item=$_POST['item'];
-    //@$codigo=$_POST['codigo'];
-    //@$descricao=$_POST['descricao'];
-    //@$quantidade=$_POST['quantidade'];
-    //@$vUnitario=$_POST['vUnitario'];
-    //@$vTotalItem=$_POST['vTotalItem'];
-    @$vPedido=$_POST['vPedido'];
-    @$vDesconto=$_POST['vDesconto'];
     
     date_default_timezone_set('America/Sao_Paulo');
 ?>
     </head>
     <body>
         <div class="conteudo">
+            <?php 
+                /*@$flash=Flash::getFlashes()[0];*/
+                if(@$flash): ?>
+            <script>var flash="<?= $flash ?>";</script>
+            <div class="erro"><?= $flash ?></div>
+            <?php else: ?>
+            <script>var flash=null;</script>
+            <?php endif ?>
             <h3><?= $razao_social ?></h3>
             <div class="web"><?= $website ?></div>
             <div class="endereco">Tel. (<?= $telefone1_ddd ?>) <?= $telefone1_numero ?> </div><br>
@@ -239,44 +255,41 @@
             <!--<div class="IM">IM:0.000.000-0</div>-->
             <hr>
             <span class="data"><?= date('d/m/Y h:m:s') ?></span>
-            <!--O CCF significa Contador de Cupom Fiscal, que serve como um contador da impressora fiscal que conta os cupons fiscais emitidos pela impressora fiscal.
-
-            O COO, Contador de Ordem de Operação, é o número mais destacado em negrito. Este número é o número do Cupom Fiscal. Os números do CCO registram o primeiro e o último documento emitidos no dia-->
             <span class="pedido">Pedido: <?= preg_replace('/^0+/','',@$numero_pedido) ?></span><br>
             <span>Vendedor: <?= @$vendedor ?></span><br>
             <hr>
-            <span class="campos">Cliente: </span><span class="dCliente"><?= $_POST['cliente'] ?></span><br>
-            <span class="campos">Endereço: </span><span class="dCliente"><?= $_POST['endereco'] ?></span><br>
-            <span class="campos">Bairro: </span><span class="dCliente"><?= $_POST['bairro'] ?></span>&nbsp&nbsp&nbsp <span class="campos">Cep: </span><span class="dCliente"><?= substr($_POST['cep'],0,4).'-'.substr($_POST['cep'],5,3) ?></span><br>
-            <span class="campos">Cidade: </span><span class="dCliente"><?= $_POST['cidade'] ?></span><br>
-            <span class="campos">CPF/CNPJ: </span><span class="dCliente"><?= $_POST['cnpj_cpf'] ?></span><br>
+            <span class="campos">Cliente: </span><span class="dCliente"><?= $cliente ?></span><br>
+            <span class="campos">Endereço: </span><span class="dCliente"><?= $enderecoCliente ?></span><br>
+            <span class="campos">Bairro: </span><span class="dCliente"><?= $bairroCliente ?></span>&nbsp&nbsp&nbsp <span class="campos">Cep: </span><span class="dCliente"><?= $cepCliente ?></span><br>
+            <span class="campos">Cidade: </span><span class="dCliente"><?= $cidadeCliente ?></span><br>
+            <span class="campos">CPF/CNPJ: </span><span class="dCliente"><?= $cnpj_cpfCliente ?></span><br>
             <hr>
-            <!--<h2>CUPOM FISCAL</h2>-->
             <div class="titulo1">
                 <span>ÍTEM &nbsp&nbsp&nbspCODIGO</span><span class="descricao">DESCRIÇÃO<br></span>
+                <span class='seriais_'>SERIAL</span><br>
                 <span class='qtd'>QTD.</span><span class=vlUnit>VL. UNIT(R$)</span><span class='vlItem'>VL. ÍTEM(R$)</span>
             </div>
             <hr>
             <div class="titulo2">
             <?php for($i=1;$i<=$tItens;$i++): 
                 $codigo=$pedido_venda_produto->det[$i-1]['produto']->codigo_produto_integracao;
-                $descricao=$pedido_venda_produto->det[$i-1]['produto']->descricao;
+                $descricao=reduz($pedido_venda_produto->det[$i-1]['produto']->descricao,40);
                 $quantidade=$pedido_venda_produto->det[$i-1]['produto']->quantidade;
                 $dados_adcionais_item=$pedido_venda_produto->det[$i-1]['inf_adic']->dados_adicionais_item;
                 $vUnitario=number_format($pedido_venda_produto->det[$i-1]['produto']->valor_unitario,'2',',','.');
                 $vTotalItem=number_format($pedido_venda_produto->det[$i-1]['produto']->valor_mercadoria,'2',',','.');
-                //print_r($dados_adcionais_item));die;
             ?>
                 <span><?= '00'.$i.'</span><span class=cod>'.$codigo.'</span><span class=desc>'.$descricao ?><br></span>
-                <span class="quant">&nbsp&nbsp<?= $quantidade.'</span> &nbsp&nbsp&nbsp&nbsp&nbsp X <span class=vlUnit2>'.$vUnitario.'</span><span class=vTotalItem2>'.$vTotalItem ?></span><br>
                 <div class="seriais"><?= nl2br($dados_adcionais_item) ?></div>
+                <span class="quant">&nbsp&nbsp<?= $quantidade.'</span> &nbsp&nbsp&nbsp&nbsp&nbsp X <span class=vlUnit2>'.$vUnitario.'</span><span class=vTotalItem2>'.$vTotalItem ?></span><br>
             <?php endfor; ?>
-            </div><br>
+            </div>
             <span class=desconto>(Desconto) -<?= $vDesconto ?></span><br>
             <hr class='hrTotal'>
             <div class='final'>
                 <span class="total">TOTAL</span>
                 <span class="vTotal">R$ <?= $vPedido ?></span>
+                <span class='fpagamento'>(FORM. PAG.) &nbsp<?= $fPagamento ?></span>
             </div>
             <br><br>
             <hr>
