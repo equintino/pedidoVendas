74c7cb1300c7bfd4bccb19b3adf046e4f44d8835
equintino <emquintino@ig.com.br>
2018-05-10 00:32:54 -0300

    tabela

---
+++ relatorios/teste.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Relatórios</title>
+        <link rel="stylesheet" type="text/css" href="../web/css/datatables.min.css"/>
+        <link rel="stylesheet" type="text/css" href="../web/css/datatables.min.css"/>
+        <script type="text/javascript" src="../web/js/datatables.min.js"></script>
+        <link type=''
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
