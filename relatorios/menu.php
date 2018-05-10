7729eec8638bb4367d016c5eff818cf39b81825f
equintino <emquintino@ig.com.br>
2018-05-10 16:46:48 -0300

    desktop

---
+++ relatorios/menu.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Consulta de Pedidos</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/consulta.css"/>
+        <script src="js/Chart.min.js"></script>
+        <?php include 'relatorio.php'; ?>
+    </head>
+    <body>
+        <div class="back"></div>
+        <div class="cabecalho">
+        <input type="checkbox" id="bt_menu"/>
+        <label for="bt_menu">&#9776;</label>
+        <nav class="menu">
+            <ul>
+                <li><a href="#">PEDIDDOS PENDENTES</a></li>
+                <li><a href="pedidos.html">PEDIDDOS VENDAS</a>
+                <ul>
+                    <li><a href="dinheiro.php">HOJE</a></li>
+                    <li><a href="debito.php">SEMANA</a></li>
+                    <li><a href="credito.php">MÊS</a></li>
+                </ul>
+                </li>
+                <li><a href="consulta.php">MOVIMENTAÇÃO</a>
+                    <ul>
+                        <li><a href="dinheiro.php">DINHEIRO</a></li>
+                        <li><a href="debito.php">DÉBITO</a></li>
+                        <li><a href="credito.php">CRÉDITO</a></li>
+                     </ul>
+                </li>
+                <li><a href="relgel.php">RELATÓRIO GERAL</a></li>
+            </ul>
+        </nav>
+        </div>
+    </body>
+</html>
---
+++ relatorios/relgel.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+        
+        <meta charset="UTF-8">
+        <title>Relatórios</title>
+        <link rel="stylesheet" type="text/css" media="screen" href="../web/css/jquery.dataTables.min.css"/>
+        <script type="text/javascript" src='../web/js/jquery-3.2.1.min.js' ></script>
+        <script type="text/javascript" src='../web/js/jquery.dataTables.min.js' ></script>
+        <script type='text/javascript'>
+            $(document).ready(function(){
+                $('#tabela1').DataTable({
+                    "order": [[ 3, "desc" ]],
+                    "columnDefs": [
+                            {
+                                "targets": [ 1 ],
+                                "visible": true,
+                                "searchable": true
+                            }
+                        ],
+                    /*"stateSave": true,*/
+                    /*"scroolX": true,*/
+                    /*"language": {
+                            "decimal": ",",
+                            "thousands": "."
+                        },*/
+                    "language": {
+                            "lengthMenu": "Exibir _MENU_ registros por página",
+                            "zeroRecords": "Nenhum registro encontrado",
+                            "info": "Exibindo pág _PAGE_ de _PAGES_",
+                            "infoEmpty": "Nenhum registro disponível",
+                            "infoFiltered": "(total de _MAX_ registros)"
+                        },
+                    "pagingType": "full_numbers",
+                });
+            });
+        </script>
+        <?php
+        include "menu.php";
+        ?>
+        <style type:"text/css">
+            #principal {
+                margin: auto;
+                margin-top: 50px;
+                width: 900px;
+            }
+        </style>
+    </head>
+    <body>
+    <div id="principal">
+    <table class="display" id='tabela1'>
+        <thead><tr><th>Nome</th><th>cpf</th><th>endereco</th><th>sexo</th><th>Valor</th></tr></thead>
+        <tbody>
+            <tr><td>edmilson</td><td>12345789</td><td>Rua josé</td><td>M</td><td>1.234.567,89</td></tr>
+            <tr><td>jorge</td><td>11111111</td><td>Rua andré</td><td>M</td><td>1234567.89</td></tr>
+            <tr><td>lucas</td><td>2222222</td><td>Rua marcos</td><td>M</td><td>1234567.89</td></tr>
+            <tr><td>gabriel</td><td>3333333</td><td>Rua antônio</td><td>M</td><td>1234567.89</td></tr>
+            <tr><td>edmilson</td><td>12345789</td><td>Rua josé</td><td>M</td><td>1234567.89</td></tr>
+            <tr><td>jorge</td><td>11111111</td><td>Rua andré</td><td>M</td><td>1234567.89</td></tr>
+            <tr><td>lucas</td><td>2222222</td><td>Rua marcos</td><td>M</td><td>1234567.89</td></tr>
+            <tr><td>gabriel</td><td>3333333</td><td>Rua antônio</td><td>M</td><td>1234567.89</td></tr>
+            <tr><td>edmilson</td><td>12345789</td><td>Rua josé</td><td>M</td><td>1234567.89</td></tr>
+            <tr><td>jorge</td><td>11111111</td><td>Rua andré</td><td>M</td><td>1234567.89</td></tr>
+            <tr><td>lucas</td><td>2222222</td><td>Rua marcos</td><td>M</td><td>1234567.89</td></tr>
+            <tr><td>gabriel</td><td>3333333</td><td>Rua antônio</td><td>M</td><td>1234567.89</td></tr>
+            <tr><td>edmilson</td><td>12345789</td><td>Rua josé</td><td>M</td><td>1234567.89</td></tr>
+            <tr><td>jorge</td><td>11111111</td><td>Rua andré</td><td>M</td><td>1234567.89</td></tr>
+            <tr><td>lucas</td><td>2222222</td><td>Rua marcos</td><td>M</td><td>1234567.89</td></tr>
+            <tr><td>gabriel</td><td>3333333</td><td>Rua antônio</td><td>M</td><td>1234567.89</td></tr>
+            <tr><td>edmilson</td><td>12345789</td><td>Rua josé</td><td>M</td><td>1234567.89</td></tr>
+            <tr><td>jorge</td><td>11111111</td><td>Rua andré</td><td>M</td><td>1234567.89</td></tr>
+            <tr><td>lucas</td><td>2222222</td><td>Rua marcos</td><td>M</td><td>1234567.89</td></tr>
+            <tr><td>gabriel</td><td>3333333</td><td>Rua antônio</td><td>M</td><td>1234567.89</td></tr>
+            <tr><td>mônica</td><td>12345789</td><td>Rua josé</td><td>S</td><td>1234567.89</td></tr>
+            <tr><td>marcela</td><td>11111111</td><td>Rua andré</td><td>S</td><td>1234567.89</td></tr>
+            <tr><td>vanessa</td><td>2222222</td><td>Rua marcos</td><td>S</td><td>1234567.89</td></tr>
+            <tr><td>maria</td><td>3333333</td><td>Rua antônio</td><td>S</td><td>1234567.89</td></tr>
+        </tbody>
+    </table>
+    </div>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
