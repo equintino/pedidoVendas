58cecc4e788a332ae16f8cfa59bfe2d944816bfa
equintino <emquintino@ig.com.br>
2018-05-13 00:54:58 -0300

    periodo

--- relatorios/relgel.php
+++ relatorios/relgel.php
@@ -4,22 +4,25 @@
         <meta charset="UTF-8">
         <title>Relatórios</title>
         <link rel="stylesheet" type="text/css" media="screen" href="../web/css/jquery.dataTables.min.css"/>
+        <link rel='stylesheet' type='text/css' href='css/jquery-ui.min.css' />
+        <link rel='stylesheet' type='text/css' href='css/jquery-ui.structure.min.css' />
+        <link rel='stylesheet' type='text/css' href='css/jquery-ui.theme.min.css' />
         <script type="text/javascript" src='../web/js/jquery-3.2.1.min.js' ></script>
         <script type="text/javascript" src='../web/js/jquery.dataTables.min.js' ></script>
+        <script type='text/javascript' src='js/jquery-ui.min.js'></script>
         <script type='text/javascript'>
             /* Custom filtering function which will search data in column four between two values */
             $.fn.dataTable.ext.search.push(
                 function( settings, data, dataIndex ) {
-                    //alert($('#min').val());
-                    var min = parseInt( $('#min').val(), 10 );
-                    var max = parseInt( $('#max').val(), 10 );
-                    //alert(max);
-                    var age = parseFloat( data[0] ) || 0; // use data for the age column
-
+                    var min = new Date($('#min').val());
+                    var max = new Date($('#max').val());
+                    /*var age = parseFloat( data[0] ) || 0;*/
+                    var dat = new Date(data[0]) || 0;
+                    
                     if ( ( isNaN( min ) && isNaN( max ) ) ||
-                         ( isNaN( min ) && age <= max ) ||
-                         ( min <= age   && isNaN( max ) ) ||
-                         ( min <= age   && age <= max ) )
+                         ( isNaN( min ) && dat <= max ) ||
+                         ( min <= dat   && isNaN( max ) ) ||
+                         ( min <= dat   && dat <= max ) )
                     {
                         return true;
                     }
@@ -27,6 +30,19 @@
                 }
             );
             $(document).ready(function(){
+                $('#min').datepicker({dateFormat: 'dd/mm/yy'});
+                $('#max').datepicker({dateFormat: 'dd/mm/yy'});
+                $('#min').change(function(){
+                    
+                    //alert(dMin);
+                    /*dataMin=filtroData($('#min').val());
+                    $('#min').attr('dMin',dataMin);
+                    alert($('#min').attr('dMin'));*/
+                })
+                /*$('#max').change(function(){
+                    dataMax=filtroData($('#max').val());
+                    $('#max').attr('dMax',dataMax);
+                })*/
                 var table = $('#tabela1').DataTable({
                     "order": [[ 0, "desc" ]],
                     "columnDefs": [
@@ -75,7 +91,8 @@
                     }
                 });
                     // Event listener to the two range filtering inputs to redraw on input
-                $('#min, #max').click( function() {
+                $('#max, #min').change( function() {
+                    //alert(filtroData($('#max').val()));
                     //alert(table);
                     table.draw();
                 } );
@@ -101,6 +118,13 @@
         function numeroParaMoeda(n, c, d, t){
             c = isNaN(c = Math.abs(c)) ? 2 : c, d = d == undefined ? "," : d, t = t == undefined ? "." : t, s = n < 0 ? "-" : "", i = parseInt(n = Math.abs(+n || 0).toFixed(c)) + "", j = (j = i.length) > 3 ? j % 3 : 0;
             return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");    
+        } 
+        function filtroData(str){
+            var ano = str.substr(-4,4);
+            var mes = str.substr(3,2);
+            var dia = str.substr(0,2);
+            var str = ano+'/'+mes+'/'+dia;
+            return str;
         }
         </script>
         <?php 
@@ -120,27 +144,31 @@
             }
             .titulo{
                 text-align: center;
+                margin-left: 200px;
                 font-family: sans-serif;
                 font-size: 30px;
             }
             .oculTitulo{
                 font-size: 18px;
             }
+            .periodo{
+                float: right;
+            }
         </style>
     </head>
     <body>
     <div id="principal">
-    <div class='titulo'>RELATÓRIO GERAL</div>
-    <table cellspacing="5" cellpadding="5" border="0">
+    <table class="periodo" cellspacing="5" cellpadding="5" border="0">
         <tbody><tr>
             <td>Data Inicio:</td>
             <td><input id="min" name="min" type="text"></td>
         </tr>
         <tr>
-            <td>Data Fim:</td>
+            <td style='text-align:right'>Data Fim:</td>
             <td><input id="max" name="max" type="text"></td>
         </tr>
     </tbody></table>
+    <div class='titulo'>RELATÓRIO GERAL</div>
     <table class="display" id="tabela1">
         <thead><tr><th>DATA</th><th>PEDIDO</th><th>VALOR DO PEDIDO</th><th>FORMA DE PAGAMENTO</th><th>N° DOCUMENTO</th><th>ETAPA</th><th>VENDEDOR</th><th>CLIENTE</th><th>QTD VOLUME</th><th>CÓD PRODUTO</th><th>DESCRIÇÃO</th><th>SERIAL</th><th>TRANSPORTADORA</th></tr></thead>
         <tbody>
