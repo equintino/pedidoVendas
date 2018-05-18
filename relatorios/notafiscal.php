6500dfd449fccd5416fcf8fe9840fd0f9c044957
equintino <emquintino@ig.com.br>
2018-05-18 01:07:22 -0300

    nota

--- relatorios/notafiscal.php
+++ relatorios/notafiscal.php
@@ -7,16 +7,25 @@
             include 'NFConsultarJsonClient.php';
             include 'relatorio.php';
             include '../model/modelNota.php';
+            include '../dao/NotaSearchCriteria.php';
         ?>
     </head>
     <body>
         <?php
            $notaOmie=new NFConsultarJsonClient();
-           $regPagina=1;
+           $regPagina=20;
            $nfListarRequest=array("pagina"=>1,"registros_por_pagina"=>$regPagina,"apenas_importado_api"=>"N","ordenar_por"=>"CODIGO");
            $dados=$notaOmie->ListarNF($nfListarRequest);
-           $cont=0;
-           foreach($dados->nfCadastro as $item_){
+           gravaDados($dados,0);
+            
+           $tPaginas=$dados->total_de_paginas;
+           for($x=2;$x<=$tPaginas;$x++){
+               $nfListarRequest=array("pagina"=>$x,"registros_por_pagina"=>$regPagina,"apenas_importado_api"=>"N","ordenar_por"=>"CODIGO");
+               $dados=$notaOmie->ListarNF($nfListarRequest);
+               gravaDados($dados,1);
+           }
+           function gravaDados($dados,$cont=null){
+               foreach($dados->nfCadastro as $item_){
                 $nota=new nota();
                 if($cont==0){
                     foreach($item_ as $key => $item){
@@ -64,11 +73,11 @@
                                  $campos[]=$key2;
                              }
                          }elseif($key=='total'){
-                             $campos[]=$key;
+                             $campos[]=trim($key);
                              foreach($item as $key2 => $item2){
-                                 $campos[]=$key2;
+                                 $campos[]=trim($key2);
                                  foreach($item2 as $key3 => $item3){
-                                     $campos[]=$key3;
+                                     $campos[]=trim($key3);
                                  }
                              }
                          }
@@ -80,10 +89,8 @@
                         $arquivos->novoArquivo($campos);
                     }
                     include '../dao/CRUDNota.php';
-                    $dao2=new CRUDNota();
                 }
                 $cont++;
-                echo '<pre>';
                 foreach($item_ as $key => $item){
                     if($key!='det' && $key!='titulos' && $key!='total'){
                         foreach($item as $key2 => $item2){
@@ -93,22 +100,48 @@
                     }elseif($key=='det'){
                         for($x=0;$x<count($item);$x++){
                             foreach($item[$x] as $key2 => $item2){
-                                $classe='set'.$key2;
-                                $nota->$classe($item2);
+                                foreach($item[$x]->$key2 as $key3 => $item3){
+                                    if($x==0){
+                                        $$key3=null;
+                                    }
+                                    $$key3 .=$item3.'*/*';
+                                }
+                            }
+                        }
+                        foreach($item[0] as $key2 => $item2){
+                            foreach($item[0]->$key2 as $key3 => $item3){
+                                $classe='set'.$key3;
+                                $nota->$classe($$key3);
+                            }
+                        }           
+                    }elseif($key=='titulos'){
+                        for($x=0;$x<count($item);$x++){
+                            foreach($item[$x] as $key2 => $item2){
+                                if($x==0){
+                                    $$key2=null;
+                                }
+                                $$key2 .=$item2.'*/*';
+                            }
+                        }
+                            if($item[0]){
+                                foreach($item[0] as $key2 => $item2){
+                                    $classe='set'.$key2;
+                                    $nota->$classe($$key2);
+                                }
+                            }
+                                
+                    }elseif($key=='total'){
+                        foreach($item as $key2 => $item2){
+                            foreach($item->$key2 as $key3 => $item3){
+                                $classe='set'.trim($key3);
+                                $nota->$classe($item3);
                             }
                         }
                     }
                 }
-                        print_r($nota);die;
-                //echo '<pre>';print_r($item_);die;
-                print_r($dao2->grava7($nota));die;
+                $dao2=new CRUDNota();
+                $dao2->grava7($nota);
             }
-           
-           $tPaginas=$dados->total_de_paginas;
-           for($x=2;$x<=$tPaginas;$x++){
-               $nfListarRequest=array("pagina"=>$x,"registros_por_pagina"=>$regPagina,"apenas_importado_api"=>"N","ordenar_por"=>"CODIGO");
-               $dados=$notaOmie->ListarNF($nfListarRequest);
-               echo '<pre>';print_r($dados);
            }
         ?>
     </body>
